c351d30be2fb5bb210a40f058b7404f04712ba29
Matus Kmit <matuskmit1@gmail.com>
2026-05-20 10:18:14 +0200

    Use shared ring-exception/wrap instead of custom wrap-catch

--- src/server/leihs/lending/routing.clj
+++ src/server/leihs/lending/routing.clj
@@ -1,23 +1,15 @@
 (ns leihs.lending.routing
   (:require
    [leihs.core.db :as db]
+   [leihs.core.ring-exception :as ring-exception]
    [leihs.lending.routes :as routes]
    [ring.middleware.content-type :refer [wrap-content-type]]
    [ring.middleware.json :refer [wrap-json-body wrap-json-response]]))
-
-(defn wrap-catch [handler]
-  (fn [req]
-    (try (handler req)
-         (catch clojure.lang.ExceptionInfo e
-           {:status (or (-> e ex-data :status) 500)
-            :body   (ex-message e)})
-         (catch Exception _
-           {:status 500 :body "Internal Server Error"}))))
 
 (defn init []
   (-> (routes/handler)
       db/wrap-tx
       (wrap-json-body {:keywords? true})
       wrap-json-response
-      wrap-catch
+      ring-exception/wrap
       wrap-content-type))
