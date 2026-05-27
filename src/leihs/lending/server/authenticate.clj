0e83c78257d1fe59d276ceba5900a305537924fb
Matus Kmit <matuskmit1@gmail.com>
2026-05-27 13:12:48 +0200

    return graphql error on unauthenticated api requests

--- src/leihs/lending/server/authenticate.clj
+++ src/leihs/lending/server/authenticate.clj
@@ -1,15 +1,17 @@
 (ns leihs.lending.server.authenticate
   (:require
    [clojure.string :as str]
+   [leihs.core.graphql.helpers :refer [error-as-graphql-object]]
    [ring.util.response :refer [redirect]]))
 
 (def skip-paths #{"/lending/graphiql" "/lending/sign-in"})
-(def api-paths #{"/lending/graphql"})
+(def graphql-path "/lending/graphql")
 
 (defn wrap [handler]
   (fn [{:keys [uri] :as request}]
     (cond
       (or (skip-paths uri) (:authenticated-entity request)) (handler request)
-      (api-paths uri) {:status 401 :headers {"Content-Type" "text/plain"} :body "Unauthorized"}
+      (= graphql-path uri) {:status 401
+                            :body (error-as-graphql-object "UNAUTHENTICATED" "Not authenticated")}
       (not (str/starts-with? uri "/lending/")) (handler request)
       :else (redirect "/lending/sign-in"))))
